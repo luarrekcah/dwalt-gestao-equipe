52356ad9fcb09f2986bc235e7242ef3347ce767a
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-10-26 13:40:16 -0500

    [UPDATE] Aparency bottom tab

--- source/screens/Main/index.js
+++ source/screens/Main/index.js
@@ -16,8 +16,8 @@ const Main = ({navigation}) => {
     <Tab.Navigator
       screenOptions={({route}) => ({
         headerShown: false,
-        tabBarShowLabel: true,
-        tabBarStyle: {backgroundColor: Colors.whitetheme.primary},
+        tabBarShowLabel: false,
+        tabBarStyle: {backgroundColor: Colors.whitetheme.primary, height: 60},
         tabBarIcon: ({focused, color, size}) => {
           let iconName;
           if (route.name === 'Home') {
@@ -31,7 +31,22 @@ const Main = ({navigation}) => {
           } else if (route.name === 'Equipe') {
             iconName = focused ? 'people' : 'people-outline';
           }
-          return <Ionicons name={iconName} size={size} color={color} />;
+          return (
+            <View
+              style={[
+                styles.centerIconsTab,
+                focused
+                  ? [styles.iconTabHome, styles.iconTabActive]
+                  : styles.iconTabHome,
+              ]}>
+              <Ionicons name={iconName} size={size} color={color} />
+              {focused || route.name === 'Home' ? (
+                ''
+              ) : (
+                <Text>{route.name}</Text>
+              )}
+            </View>
+          );
         },
         tabBarActiveTintColor: '#fff',
         tabBarInactiveTintColor: '#c9c9c9',
@@ -39,26 +54,7 @@ const Main = ({navigation}) => {
       initialRouteName="Home">
       <Tab.Screen name="Empresa" component={Business} />
       <Tab.Screen name="Equipe" component={Home} />
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={({route}) => ({
-          tabBarIcon: ({focused, color, size}) => (
-            <View
-              style={
-                focused
-                  ? [styles.iconTabHome, styles.iconTabActive]
-                  : styles.iconTabHome
-              }>
-              <Ionicons
-                name={focused ? 'home' : 'home-outline'}
-                size={size}
-                color={color}
-              />
-            </View>
-          ),
-        })}
-      />
+      <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Chamados" component={Home} />
       <Tab.Screen name="Usuário" component={User} />
     </Tab.Navigator>
@@ -67,19 +63,21 @@ const Main = ({navigation}) => {
 
 const styles = StyleSheet.create({
   iconTabHome: {
-    borderRadius: 30,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
     shadowOffset: {width: 0, height: 2},
     shadowOpacity: 0.2,
     shadowRadius: 5,
-    backgroundColor: Colors.whitetheme.primary,
   },
   iconTabActive: {
-    marginBottom: 20,
     width: 60,
     height: 60,
+    backgroundColor: Colors.whitetheme.primaryDark,
+  },
+  centerIconsTab: {
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
   },
 });
 
