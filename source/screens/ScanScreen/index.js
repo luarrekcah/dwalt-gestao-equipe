0cd2758803cdece8dba3ac8fca70a33dc497f65b
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-01-07 19:26:28 -0500

    [added] add photos to survey call for end it

--- source/screens/ScanScreen/index.js
+++ source/screens/ScanScreen/index.js
@@ -5,34 +5,89 @@ import {
   Text,
   Vibration,
   TouchableOpacity,
+  ToastAndroid,
+  ActivityIndicator,
 } from 'react-native';
 import {CameraScreen} from 'react-native-camera-kit';
 import Colors from '../../global/colorScheme';
-import {getUserData, updateItem} from '../../services/Database';
+import {
+  createItem,
+  getAllItems,
+  getUserData,
+  updateItem,
+} from '../../services/Database';
+import ImagePicker from 'react-native-image-crop-picker';
+import storage from '@react-native-firebase/storage';
 
 const ScanScreen = ({navigation}) => {
   const [readed, setReaded] = React.useState(false);
-  const [data, setData] = React.useState(false);
+  const [data, setData] = React.useState();
   const [user, setUser] = React.useState();
+  const [allMedia, setAllmedia] = React.useState([]);
+  const [loadingMedia, setLoadingMedia] = React.useState(true);
 
-  const loadData = async () => {
+  const loadUser = async () => {
     setUser(await getUserData());
   };
 
   React.useEffect(() => {
-    loadData();
+    loadUser();
   }, []);
 
-  const onRead = e => {
+  const loadData = async key => {
+    if (!key) {
+      return console.warn('No key');
+    }
+    setAllmedia(
+      await getAllItems({
+        path: `gestaoempresa/business/${user.data.businessKey}/surveys/${key}/photos`,
+      }),
+    );
+    setLoadingMedia(false);
+  };
+
+  const dic = {
+    endSurvey: 'Procedimentos para concluir chamado',
+  };
+
+  const onRead = async e => {
     Vibration.vibrate(200);
     const dataCode = JSON.parse(e.nativeEvent.codeStringValue);
     if (dataCode) {
       setReaded(true);
       setData(dataCode);
       console.log(dataCode);
+      await loadData(dataCode.key);
     } else {
       console.warn('Erro');
     }
+  };
+
+  const pickImages = key => {
+    ImagePicker.openPicker({
+      includeBase64: true,
+      multiple: true,
+    }).then(async images => {
+      setLoadingMedia(true);
+      ToastAndroid.show('Enviando fotos, aguarde...', ToastAndroid.LONG);
+      for (let index = 0; index < images.length; index++) {
+        const path = `/gestaoempresa/business/${
+          user.data.businessKey
+        }/surveys/${key}/photos/${new Date().getTime()}-${index}.jpg`;
+        const reference = storage().ref(path);
+        const dataUrl = `data:image/png;base64,${images[index].data}`;
+        await reference.putString(dataUrl, 'data_url');
+        const url = await reference.getDownloadURL();
+
+        createItem({
+          path: `/gestaoempresa/business/${user.data.businessKey}/surveys/${key}/photos`,
+          params: {url, path},
+        });
+
+        ToastAndroid.show(`Foto ${index + 1} enviada.`, ToastAndroid.SHORT);
+      }
+      await loadData(key);
+    });
   };
 
   const Options = () => {
@@ -40,16 +95,37 @@ const ScanScreen = ({navigation}) => {
       return (
         <View>
           <TouchableOpacity
-            style={{
-              backgroundColor: Colors.whitetheme.primary,
-              borderRadius: 30,
-              alignItems: 'center',
-              alignContent: 'center',
-              paddingVertical: 20,
-            }}
-            onPress={() => concludeSurvey(data.key)}>
-            <Text style={{color: '#fff'}}>Fechar chamado</Text>
+            style={styles.button}
+            onPress={() => pickImages(data.key)}>
+            {loadingMedia ? (
+              <ActivityIndicator size="small" color="#fff" />
+            ) : (
+              <Text style={{color: '#fff'}}>
+                Adicionar fotos ({allMedia.length} de 5)
+              </Text>
+            )}
           </TouchableOpacity>
+          {allMedia.length >= 5 ? (
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => concludeSurvey(data.key)}>
+              <Text style={{color: '#fff'}}>Concluir chamado</Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              style={[
+                styles.button,
+                {backgroundColor: Colors.whitetheme.gray, opacity: 0.5},
+              ]}
+              onPress={() =>
+                ToastAndroid.show(
+                  'Preencha todos os campos acima para finalizar.',
+                  ToastAndroid.LONG,
+                )
+              }>
+              <Text style={{color: '#fff'}}>Concluir chamado (Bloqueado)</Text>
+            </TouchableOpacity>
+          )}
         </View>
       );
     }
@@ -64,6 +140,7 @@ const ScanScreen = ({navigation}) => {
         status: 'Chamado concluído',
       },
     });
+    ToastAndroid.show('Chamado concluído', ToastAndroid.LONG);
     navigation.goBack();
   };
 
@@ -81,7 +158,7 @@ const ScanScreen = ({navigation}) => {
                   color: Colors.whitetheme.primary,
                   marginVertical: 40,
                 }}>
-                Opções
+                {dic[data.type]}
               </Text>
               <Options />
             </View>
@@ -106,6 +183,14 @@ const styles = new StyleSheet.create({
   container: {
     padding: 10,
     paddingBottom: 100,
+  },
+  button: {
+    backgroundColor: Colors.whitetheme.primary,
+    borderRadius: 30,
+    alignItems: 'center',
+    alignContent: 'center',
+    paddingVertical: 20,
+    marginTop: 20,
   },
 });
 
